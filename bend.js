f03f9cb3b11104365da2c664decd905c3217b86c
Javin123 <97555231+Javin123@users.noreply.github.com>
2022-01-12 04:52:09 +0800

    Add files via upload

---
+++ bend.js
@@ -0,0 +1,13 @@
+function myfunction(){
+    alert("Hello Jol");
+    if(confirm("Would you want to be more than friend for me?")){
+        if(confirm("Wanna be my girlfriend?")){
+            alert("Yeayyyyy")
+            alert("Show me this message")
+        }else{
+            alert("Just press \"ok\" and nothing really happen")
+        }
+    }else{
+        alert("Its fine just press \"ok\" and nothing really happen")
+    }
+}
